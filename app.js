ce0e70e23d5b2e28e23a254206076269249fa15d
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-24 13:55:50 -0400

    Set up bodyParser

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 const weekdayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     var today = new Date();
@@ -18,6 +19,10 @@ app.get("/", (req, res) => {
     var currentDay = today.toLocaleDateString("en-US", dateOptions);
 
     res.render("list", { kindOfDay: currentDay });
+});
+
+app.post("/", (req, res) => {
+    res.send(req.body.newItem);
 });
 
 app.listen(PORT, () => {
