7eb32eef2e02deb8bfd33ba6e4da48b0d1b1296a
Justin Cole <19556622+Jcole429@users.noreply.github.com>
2023-03-24 23:23:48 -0400

    Add work list

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = 3000;
 const weekdayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = []
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,8 +26,19 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    items.push(req.body.newItem);
-    res.redirect("/");
+    console.log(req.body);
+    let item = req.body.newItem;
+    if (req.body.list === "Work List") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", (req, res) => {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.listen(PORT, () => {
